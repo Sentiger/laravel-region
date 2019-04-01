3d6f8e8f70f672ab26f3d9e24643b2ba3271386c
shiwh <shi_wenhu@qq.com>
2019-04-01 09:56:16 +0800

    test

--- src/Region.php
+++ src/Region.php
@@ -2,34 +2,11 @@
 
 namespace Yiche\Region;
 
+
 class Region
 {
-    public function getAll()
+    public static function test()
     {
-        //todo 可以加入缓存
-        return \Yiche\Region\Models\Region::get();
+        echo "test";
     }
-
-    public function getAllFormatTree()
-    {
-        return self::tree($this->getAll()->toArray(), 'children', '', 'parent_code', 'code');
-    }
-
-    public function getOneByCode($code)
-    {
-        return \Yiche\Region\Models\Region::where('code', $code)->first();
-    }
-
-    public function tree($list, $name = 'children', $pid = 0, $pidField = 'pid', $selfId = 'id')
-    {
-        $arr = [];
-        foreach ($list as $k => $v) {
-            if ($v[$pidField] == $pid) {
-                $v[$name] = self::tree($list, $name, $v[$selfId], $pidField, $selfId);
-                $arr[]    = $v;
-            }
-        }
-        return $arr;
-    }
-
 }
--- src/routes.php
+++
@@ -1,2 +0,0 @@
-<?php
-Route::get('api/region/all', \Yiche\Region\Http\Controllers\RegionController::class . '@all');
